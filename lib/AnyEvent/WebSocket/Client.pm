97e366b13b0eb90163ab8aa55bf5d2dbbda2a44b
Graham Ollis <plicease@cpan.org>
2013-09-22 07:47:56 +0000

    Import of PLICEASE/AnyEvent-WebSocket-Client-0.08 from CPAN.
    gitpan-cpan-distribution: AnyEvent-WebSocket-Client
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         PLICEASE/AnyEvent-WebSocket-Client-0.08.tar.gz
    gitpan-cpan-author:       PLICEASE
    gitpan-cpan-maturity:     released

--- lib/AnyEvent/WebSocket/Client.pm
+++ lib/AnyEvent/WebSocket/Client.pm
@@ -13,7 +13,7 @@ use AnyEvent::WebSocket::Connection;
 use PerlX::Maybe qw( maybe provided );
 
 # ABSTRACT: WebSocket client for AnyEvent
-our $VERSION = '0.07'; # VERSION
+our $VERSION = '0.08'; # VERSION
 
 
 has timeout => (
@@ -151,7 +151,7 @@ AnyEvent::WebSocket::Client - WebSocket client for AnyEvent
 
 =head1 VERSION
 
-version 0.07
+version 0.08
 
 =head1 SYNOPSIS
 
--- lib/AnyEvent/WebSocket/Connection.pm
+++ lib/AnyEvent/WebSocket/Connection.pm
@@ -9,7 +9,7 @@ use Protocol::WebSocket::Frame;
 use Scalar::Util qw( weaken );
 
 # ABSTRACT: WebSocket connection for AnyEvent
-our $VERSION = '0.07'; # VERSION
+our $VERSION = '0.08'; # VERSION
 
 
 has _stream => (
@@ -102,7 +102,7 @@ AnyEvent::WebSocket::Connection - WebSocket connection for AnyEvent
 
 =head1 VERSION
 
-version 0.07
+version 0.08
 
 =head1 SYNOPSIS
 
